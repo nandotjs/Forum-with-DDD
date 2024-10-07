e2c0867614534e0d15d6fc11d82df81a3ab1e175
nan <ipfilus@gmail.com>
2024-10-07 17:58:10 -0300

    feat: add createdAt and updatedAt menagement to entities

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
@@ -11,10 +12,10 @@ export class AnswerQuestionUseCase {
     constructor(private answersRepository: AnswersRepository) {}
 
     async execute({ instructorId, questionId, content }: AnswerQuestionRequest) {
-        const answer = new Answer({ 
+        const answer = Answer.create({ 
             content, 
-            authorId: instructorId, 
-            questionId 
+            authorId: new UniqueEntityID(instructorId), 
+            questionId: new UniqueEntityID(questionId)
         });
 
         await this.answersRepository.create(answer);
