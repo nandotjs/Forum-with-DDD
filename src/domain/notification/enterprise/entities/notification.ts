45ef5afc108b1bc5528a9d65ccb84a734a49e5a5
nan <ipfilus@gmail.com>
2024-11-20 17:37:57 -0300

    feat: add notification subdomain and notificatoin entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,43 @@
+import { AggregateRoot } from "@/core/entities/aggregate-root";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+interface NotificationProps {
+    recipientId: UniqueEntityID;
+    title: string;
+    content: string;
+    createdAt: Date;
+    readAt?: Date;
+}
+
+export class Notification extends AggregateRoot<NotificationProps> {
+    get recipientId() {
+        return this.props.recipientId;
+    }
+
+    get title() {
+        return this.props.title;
+    }
+
+    get content() {
+        return this.props.content;
+    }
+
+    get createdAt() {
+        return this.props.createdAt;
+    }
+
+    get readAt() {
+        return this.props.readAt;
+    }
+
+    static create(props: Optional<NotificationProps, 'createdAt'>, id?: UniqueEntityID) {
+        const notification = new Notification({
+            ...props,
+            createdAt: props.createdAt ?? new Date(),
+        }, id);
+
+        return notification;
+    }
+}
